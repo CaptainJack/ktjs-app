59afa2b2ba3ec2f2161d31dc7609b295af656df8
Alexey Chuprin <a.chuprin@capjack.ru>
2018-11-29 01:03:51 +0300

    Fix SoundFlowSettings

--- src/main/kotlin/ru/capjack/ktjs/app/sound/SoundFlowSettings.kt
+++ src/main/kotlin/ru/capjack/ktjs/app/sound/SoundFlowSettings.kt
@@ -1,11 +1,11 @@
 package ru.capjack.ktjs.app.sound
 
 data class SoundFlowSettings(
-	val loop: Boolean = false,
-	val start: Double = 0.0,
-	val end: Double = 0.0,
-	val restart: Double = 0.0,
-	val volume: Double = 1.0
+	var loop: Boolean = false,
+	var start: Double = 0.0,
+	var end: Double = 0.0,
+	var restart: Double = 0.0,
+	var volume: Double = 1.0
 ) {
 	companion object {
 		val DEFAULT = SoundFlowSettings()
