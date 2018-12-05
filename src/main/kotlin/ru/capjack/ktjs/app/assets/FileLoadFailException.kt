a7d2f130bd94cd1832f8e5323d54e19cde2b8a8d
Alexey Chuprin <a.chuprin@capjack.ru>
2018-12-05 14:34:05 +0300

    Develop

--- src/main/kotlin/ru/capjack/ktjs/app/assets/FileLoadFailException.kt
+++ src/main/kotlin/ru/capjack/ktjs/app/assets/FileLoadFailException.kt
@@ -4,4 +4,4 @@ import ru.capjack.ktjs.common.js.convertErrorEventToString
 import ru.capjack.ktjs.common.rl.Url
 
 class FileLoadFailException(url: Url, message: dynamic) :
-	RuntimeException("Failed to load image '$url' (${if (message is String) message.unsafeCast<String>() else convertErrorEventToString(message)})")
+	RuntimeException("Failed to load file '$url' (${if (message is String) message.unsafeCast<String>() else convertErrorEventToString(message)})")
